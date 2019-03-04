063178cdc851b7090e22cfb0123933665aa7155a
Johan Alvarez <llstarscreamll@hotmail.com>
2019-03-03 22:04:19 -0500

    Exposing abstract base service on shared lib

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/shared.module';
 export * from './lib/interfaces/api-error';
 export * from './lib/services/local-storage.service';
 export * from './lib/utils/common-functions';
+export * from './lib/abstracts/base.service';
