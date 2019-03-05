c9cbe2ae8f7686225e5683ea1f88514d0a62e352
Johan Alvarez <llstarscreamll@hotmail.com>
2019-03-04 22:51:11 -0500

    Create file with work shift mock

---
+++ libs/work-shifts/data-access/src/lib/mocks.ts
@@ -0,0 +1,12 @@
+export const WORK_SHIFT_MOCK = {
+  id: 1,
+  name: '2-10',
+  start_time: '14:00',
+  end_time: '22:00',
+  grace_minutes_for_start_time: 10,
+  grace_minutes_for_end_time: 15,
+  meal_time_in_minutes: 45,
+  min_minutes_required_to_discount_meal_time: 60 * 6,
+  created_at: '1999-01-01 01:00:00',
+  updated_at: '1999-01-01 01:00:00'
+};
