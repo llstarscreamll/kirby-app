744c2d703cf71b6412965dc4fa84f392b87bc5bd
Johan Alvarez <llstarscreamll@hotmail.com>
2019-03-28 00:16:14 -0500

    implement util function to create empty pagination objects on shared util module

--- libs/shared/src/lib/utils/common-functions.ts
+++ libs/shared/src/lib/utils/common-functions.ts
@@ -1,9 +1,17 @@
 import { get, toArray } from "lodash";
 import { ApiError } from "../interfaces/api-error";
+import { Pagination } from '../interfaces/pagination';
 
 /**
  * Maps API errors (mainly validation errors) to flat string array.
  */
 export function flatApiErrors(apiErrors: ApiError): string[] {
   return [].concat(...toArray(get(apiErrors, 'error.errors')));
+}
+
+/**
+ * Create empty pagination object.
+ */
+export function emptyPagination(): Pagination<any> {
+  return { data: [], meta: {} };
 }
