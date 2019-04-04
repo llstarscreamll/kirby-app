2db2cde56839f69d32ec837141ed8348f2b7acd2
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-03 22:16:29 -0500

    expose only facade and module from work shifts data access lib

--- libs/work-shifts/data-access/src/index.ts
+++ libs/work-shifts/data-access/src/index.ts
@@ -1,4 +1,2 @@
 export * from './lib/+state/work-shifts.facade';
-export * from './lib/+state/work-shifts.reducer';
-export * from './lib/+state/work-shifts.selectors';
 export * from './lib/work-shifts-data-access.module';
