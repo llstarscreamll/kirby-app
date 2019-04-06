c7ef7092811a0da84bc2b154909fe56511ab639c
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-06 06:21:26 -0500

    polish work shifts table list component

--- libs/work-shifts/ui/src/lib/work-shifts-table-list/work-shifts-table-list.component.spec.ts
+++ libs/work-shifts/ui/src/lib/work-shifts-table-list/work-shifts-table-list.component.spec.ts
@@ -32,16 +32,17 @@ describe('WorkShiftsTableListComponent', () => {
     fixture.detectChanges();
 
     const template: HTMLDivElement = fixture.nativeElement;
+    const tableRow = 'table tr:first-child';
     expect(template.querySelectorAll('table tbody tr').length).toBe(itemsList.length);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(1)').textContent).toContain(itemsList[0].name);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(2)').textContent).toContain(itemsList[0].start_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(3)').textContent).toContain(itemsList[0].end_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(4)').textContent).toContain(itemsList[0].grace_minutes_for_start_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(5)').textContent).toContain(itemsList[0].grace_minutes_for_end_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(6)').textContent).toContain(itemsList[0].meal_start_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(7)').textContent).toContain(itemsList[0].meal_time_in_minutes);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(8)').textContent).toContain(itemsList[0].min_minutes_required_to_discount_meal_time);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(9)').textContent).toContain(itemsList[0].created_at);
-    expect(template.querySelector('table tbody tr:first-child td:nth-child(10)').textContent).toContain(itemsList[0].updated_at);
+    expect(template.querySelector(`${tableRow} td:nth-child(1)`).textContent).toContain(itemsList[0].name);
+    expect(template.querySelector(`${tableRow} td:nth-child(2)`).textContent).toContain(itemsList[0].start_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(3)`).textContent).toContain(itemsList[0].end_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(4)`).textContent).toContain(itemsList[0].meal_start_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(5)`).textContent).toContain(itemsList[0].grace_minutes_for_start_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(6)`).textContent).toContain(itemsList[0].grace_minutes_for_end_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(7)`).textContent).toContain(itemsList[0].meal_time_in_minutes);
+    expect(template.querySelector(`${tableRow} td:nth-child(8)`).textContent).toContain(itemsList[0].min_minutes_required_to_discount_meal_time);
+    expect(template.querySelector(`${tableRow} td:nth-child(9)`).textContent).toBeTruthy();
+    expect(template.querySelector(`${tableRow} td:nth-child(10)`).textContent).toBeTruthy();
   });
 });
