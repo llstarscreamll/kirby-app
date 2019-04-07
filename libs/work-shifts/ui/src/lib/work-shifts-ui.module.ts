3fa847834b2cca74eed8a1304b7e0b91181d4d90
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-06 23:54:51 -0500

    declare new components and import some material modules on work shift ui lib

--- libs/work-shifts/ui/src/lib/work-shifts-ui.module.ts
+++ libs/work-shifts/ui/src/lib/work-shifts-ui.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkShiftsTableListComponent } from './work-shifts-table-list/work-shifts-table-list.component';
+import { WorkShiftFormComponent } from './work-shift-form/work-shift-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [WorkShiftsTableListComponent],
-  exports: [WorkShiftsTableListComponent]
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [WorkShiftsTableListComponent, WorkShiftFormComponent],
+  exports: [WorkShiftsTableListComponent, WorkShiftFormComponent]
 })
 export class WorkShiftsUiModule { }
