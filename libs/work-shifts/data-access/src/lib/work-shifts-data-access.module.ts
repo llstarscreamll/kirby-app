353fedf7d3085d2c6919c74545220dcf07e2e360
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-06 23:53:13 -0500

    remove HttpClientModule fromwork shifts data access lib, said module should be imported only on root module

--- libs/work-shifts/data-access/src/lib/work-shifts-data-access.module.ts
+++ libs/work-shifts/data-access/src/lib/work-shifts-data-access.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 
 import { WorkShiftService } from './work-shift.service';
 import { WorkShiftsEffects } from './+state/work-shifts.effects';
@@ -12,7 +11,6 @@ import { WORK_SHIFTS_FEATURE_KEY, initialState as workShiftsInitialState, workSh
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule,
     StoreModule.forFeature(WORK_SHIFTS_FEATURE_KEY, workShiftsReducer, {
       initialState: workShiftsInitialState
     }),
