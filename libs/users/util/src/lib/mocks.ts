513f577bbb7200cf18d465f932a951c381b2461b
Johan Alvarez <llstarscreamll@hotmail.com>
2019-04-15 22:55:29 -0500

    feat(users): create utils lib

---
+++ libs/users/util/src/lib/mocks.ts
@@ -0,0 +1,24 @@
+import * as faker from "faker";
+import { UserInterface } from './user.interface';
+
+export const WORK_SHIFT_MOCK: UserInterface = {
+  id: '1',
+  name: 'Tony Stark',
+  email: 'tony@stark.com',
+  email_verified_at: '1999-01-01 02:00:00',
+  created_at: '1999-01-01 01:00:00',
+  updated_at: '1999-01-01 01:00:00',
+  deleted_at: null
+};
+
+export function createUser(id: string, name: string = null): UserInterface {
+  const date = faker.date.past();
+  return {
+    id,
+    name: name || faker.random.word(),
+    email: faker.email(),
+    email_verified_at: date,
+    created_at: date,
+    updated_at: date,
+  };
+}
---
+++ libs/users/util/src/lib/user.interface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  id?: string;
+  name: string;
+  email: string;
+  password?: string;
+  email_verified_at?: string;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
