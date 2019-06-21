8199ba7fda453bb96bc5d85833b6e35937847e70
Johan Alvarez <llstarscreamll@hotmail.com>
2019-06-21 11:58:42 -0500

    feat(novelties): create new novelties utils and data libs

---
+++ libs/novelties/data/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'novelties-data',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/libs/novelties/data'
+};
---
+++ libs/novelties/data/src/lib/novelty.interface.ts
@@ -0,0 +1,10 @@
+export interface NoveltyInterface {
+  id: number;
+  time_clock_log_id: string;
+  employee_id: string;
+  novelty_type_id: string;
+  total_time_in_minutes: number;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
