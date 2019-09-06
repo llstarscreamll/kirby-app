d7c239c10233b07f9d268eceae70af988748206c
Johan Alvarez <llstarscreamll@hotmail.com>
2019-09-05 23:58:48 -0500

    feat(novelties): add missing attributes to novelty interface

--- libs/novelties/data/src/lib/novelty.interface.ts
+++ libs/novelties/data/src/lib/novelty.interface.ts
@@ -1,10 +1,16 @@
+import { EmployeeInterface } from '@llstarscreamll/employees/util';
+import { NoveltyTypeInterface } from '@llstarscreamll/novelty-types/data';
+
 export interface NoveltyInterface {
   id: number;
   time_clock_log_id: string;
   employee_id: string;
+  employee?: EmployeeInterface;
   novelty_type_id: string;
+  novelty_type?: NoveltyTypeInterface;
+  start_at?: string;
+  end_at?: string;
   total_time_in_minutes: number;
-  novelty_type: any;
   created_at?: string;
   updated_at?: string;
   deleted_at?: string;
