4e5faf8991a84c46fd5404bbe974cd5239cd8690
Johan Alvarez <llstarscreamll@hotmail.com>
2019-09-07 15:01:12 -0500

    test: make time-clock-logs-table.component tests green

--- libs/time-clock-logs/ui/src/lib/time-clock-logs-table/time-clock-logs-table.component.spec.ts
+++ libs/time-clock-logs/ui/src/lib/time-clock-logs-table/time-clock-logs-table.component.spec.ts
@@ -1,29 +1,33 @@
-import * as moment from "moment";
+import moment from 'moment';
+import { isArray } from 'lodash';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ChangeDetectionStrategy, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { createUser } from '@llstarscreamll/users/util';
+import { createNovelty } from '@llstarscreamll/novelties/utils';
 import { createTimeClockLog } from '@llstarscreamll/time-clock-logs/util';
 import { TimeClockLogsTableComponent } from './time-clock-logs-table.component';
-import { createNovelty } from '@llstarscreamll/novelties/utils';
-import { createUser } from '@llstarscreamll/users/util';
-import { isArray } from 'lodash';
-import { RouterModule } from '@angular/router';
 
 describe('TimeClockLogsTableComponent', () => {
   let template: HTMLDivElement;
   let component: TimeClockLogsTableComponent;
   let fixture: ComponentFixture<TimeClockLogsTableComponent>;
-  const approveButtonSelector = 'table tbody tr:first-child td:last-child .approve';
-  const deleteApprovalButtonSelector = 'table tbody tr:first-child td:last-child .delete-approval';
+  const approveButtonSelector =
+    'table tbody tr:first-child td:last-child .approve';
+  const deleteApprovalButtonSelector =
+    'table tbody tr:first-child td:last-child .delete-approval';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterModule.forChild([])],
+      imports: [RouterTestingModule],
       declarations: [TimeClockLogsTableComponent],
       schemas: [NO_ERRORS_SCHEMA]
-    }).overrideComponent(TimeClockLogsTableComponent, {
-      set: { changeDetection: ChangeDetectionStrategy.Default }
-    }).compileComponents();
+    })
+      .overrideComponent(TimeClockLogsTableComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default }
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -48,14 +52,18 @@ describe('TimeClockLogsTableComponent', () => {
       6: 'H. salida',
       7: 'Novedades',
       8: 'Aprobaciones',
-      9: '',
+      9: ''
     };
 
     fixture.detectChanges();
 
     expect(template.querySelectorAll('table thead tr').length).toBe(1);
     Object.keys(theadRowMap).forEach(key => {
-      expect(template.querySelector(`table thead tr:first-child th:nth-child(${key})`).textContent).toContain(theadRowMap[key]);
+      expect(
+        template.querySelector(
+          `table thead tr:first-child th:nth-child(${key})`
+        ).textContent
+      ).toContain(theadRowMap[key]);
     });
   });
 
@@ -67,26 +75,26 @@ describe('TimeClockLogsTableComponent', () => {
     timeClockLog.approvals = [tony, steve];
     timeClockLog.novelties = [
       createNovelty(null, { time_clock_log_id: timeClockLog.id }),
-      createNovelty(null, { time_clock_log_id: timeClockLog.id }),
+      createNovelty(null, { time_clock_log_id: timeClockLog.id })
     ];
     timeClockLog.novelties_count = timeClockLog.novelties.length;
 
     component.timeClockLogs = {
-      data: [
-        timeClockLog,
-        createTimeClockLog(),
-      ],
+      data: [timeClockLog, createTimeClockLog()],
       meta: {}
     };
 
     const firstTbodyRowMap = {
       1: timeClockLog.id,
-      2: timeClockLog.employee.user.first_name + ' ' + timeClockLog.employee.user.last_name,
+      2:
+        timeClockLog.employee.user.first_name +
+        ' ' +
+        timeClockLog.employee.user.last_name,
       3: timeClockLog.sub_cost_center.name,
       4: timeClockLog.work_shift.name,
       5: moment(timeClockLog.checked_in_at).format('YY-MM-DD HH:mm'),
       6: moment(timeClockLog.checked_out_at).format('YY-MM-DD HH:mm'),
-      7: timeClockLog.concatenatedNoveltiesCount,
+      7: timeClockLog.novelties.map(n => n.novelty_type.code),
       8: ['Tony Stark', 'Steve Rogers']
     };
 
@@ -112,12 +120,14 @@ describe('TimeClockLogsTableComponent', () => {
     firstTimeClockLog.approvals = [approver];
     component.userId = approver.id;
     component.timeClockLogs = {
-      data: [firstTimeClockLog, createTimeClockLog(),],
+      data: [firstTimeClockLog, createTimeClockLog()],
       meta: {}
     };
 
-    const approveButtonSelector = 'table tbody tr:last-child td:last-child .approve';
-    const deleteApprovalButtonSelector = 'table tbody tr:first-child td:last-child .delete-approval';
+    const approveButtonSelector =
+      'table tbody tr:last-child td:last-child .approve';
+    const deleteApprovalButtonSelector =
+      'table tbody tr:first-child td:last-child .delete-approval';
 
     // show buttons
     component.actionButtons = ['approve', 'delete-approval'];
@@ -147,7 +157,7 @@ describe('TimeClockLogsTableComponent', () => {
   it('should emit values when approve action button is clicked', () => {
     const firstTimeClockLog = createTimeClockLog();
     component.timeClockLogs = {
-      data: [firstTimeClockLog, createTimeClockLog(),],
+      data: [firstTimeClockLog, createTimeClockLog()],
       meta: {}
     };
 
@@ -156,7 +166,9 @@ describe('TimeClockLogsTableComponent', () => {
 
     fixture.detectChanges();
 
-    const approveButton: HTMLButtonElement = template.querySelector(approveButtonSelector);
+    const approveButton: HTMLButtonElement = template.querySelector(
+      approveButtonSelector
+    );
     approveButton.click();
 
     expect(component.approve.emit).toHaveBeenCalledWith(firstTimeClockLog.id);
@@ -168,7 +180,7 @@ describe('TimeClockLogsTableComponent', () => {
     firstTimeClockLog.approvals = [approver];
     component.userId = approver.id;
     component.timeClockLogs = {
-      data: [firstTimeClockLog, createTimeClockLog(),],
+      data: [firstTimeClockLog, createTimeClockLog()],
       meta: {}
     };
 
@@ -177,10 +189,14 @@ describe('TimeClockLogsTableComponent', () => {
 
     fixture.detectChanges();
 
-    const deleteApprovalButton: HTMLButtonElement = template.querySelector(deleteApprovalButtonSelector);
+    const deleteApprovalButton: HTMLButtonElement = template.querySelector(
+      deleteApprovalButtonSelector
+    );
     deleteApprovalButton.click();
 
-    expect(component.deleteApproval.emit).toHaveBeenCalledWith(firstTimeClockLog.id);
+    expect(component.deleteApproval.emit).toHaveBeenCalledWith(
+      firstTimeClockLog.id
+    );
   });
 
   it('should hide approve and show delete-approval button when row is approved by user', () => {
@@ -194,8 +210,10 @@ describe('TimeClockLogsTableComponent', () => {
       meta: {}
     };
 
-    const approveButtonSelector = 'table tbody tr:first-child td:last-child .approve';
-    const deleteApprovalButtonSelector = 'table tbody tr:first-child td:last-child .delete-approval';
+    const approveButtonSelector =
+      'table tbody tr:first-child td:last-child .approve';
+    const deleteApprovalButtonSelector =
+      'table tbody tr:first-child td:last-child .delete-approval';
 
     // show buttons
     component.actionButtons = ['approve', 'delete-approval'];
@@ -217,8 +235,10 @@ describe('TimeClockLogsTableComponent', () => {
       meta: {}
     };
 
-    const approveButtonSelector = 'table tbody tr:first-child td:last-child .approve';
-    const deleteApprovalButtonSelector = 'table tbody tr:first-child td:last-child .delete-approval';
+    const approveButtonSelector =
+      'table tbody tr:first-child td:last-child .approve';
+    const deleteApprovalButtonSelector =
+      'table tbody tr:first-child td:last-child .delete-approval';
 
     // show buttons
     component.actionButtons = ['approve', 'delete-approval'];
@@ -228,5 +248,4 @@ describe('TimeClockLogsTableComponent', () => {
     expect(template.querySelector(approveButtonSelector)).toBeTruthy(); // user has not approved first row data
     expect(template.querySelector(deleteApprovalButtonSelector)).toBeFalsy(); // user can't delete approval
   });
-
 });
