805e2bf3c0220c43fd4d26c9842eb3a1d23b856a
Johan Alvarez <llstarscreamll@hotmail.com>
2019-09-23 13:12:01 -0500

    style: change some test describers text

--- libs/novelties/data/src/lib/novelty.model.spec.ts
+++ libs/novelties/data/src/lib/novelty.model.spec.ts
@@ -1,4 +1,4 @@
-describe('UserInterface', () => {
+describe('NoveltyInterface', () => {
   // this is to satisfy jest and prevent to exit with code 0
   it('contains a empty test', () => {
     expect(true).toBe(true);
