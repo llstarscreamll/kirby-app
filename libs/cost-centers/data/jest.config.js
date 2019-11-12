662152a9ec6d7c7d10bd1c233cf4439514d84c7a
Johan Alvarez <llstarscreamll@hotmail.com>
2019-11-11 23:53:42 -0500

    feat(cost-centers): new data cost centers library

---
+++ libs/cost-centers/data/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'cost-centers-data',
+  preset: '../../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../../coverage/libs/cost-centers/data'
+};
