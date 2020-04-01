44b896bc6cc419fadc54df412adce655106da83d
Johan Alvarez <llstarscreamll@hotmail.com>
2020-03-31 21:28:10 -0500

    tests(novelties): make default report by employeee page test green

--- libs/novelties/feature/src/lib/report-by-employee-page/report-by-employee-page.component.spec.ts
+++ libs/novelties/feature/src/lib/report-by-employee-page/report-by-employee-page.component.spec.ts
@@ -1,5 +1,14 @@
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { USER } from '@kirby/authentication/utils';
+import { AuthFacade } from '@kirby/authentication-data-access';
+import { NoveltiesFacade } from '@kirby/novelties/data-access';
+import { EmployeesFacade } from '@kirby/employees/data-access';
 import { ReportByEmployeePageComponent } from './report-by-employee-page.component';
 
 describe('ReportByEmployeePageComponent', () => {
@@ -8,9 +17,19 @@ describe('ReportByEmployeePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReportByEmployeePageComponent ]
-    })
-    .compileComponents();
+      imports: [MatAutocompleteModule, ReactiveFormsModule],
+      declarations: [ReportByEmployeePageComponent],
+      providers: [
+        { provide: AuthFacade, useValue: { authUser$: of(USER) } },
+        {
+          provide: NoveltiesFacade,
+          useValue: { reportByEmployee$: of(), error$: of() }
+        },
+        { provide: EmployeesFacade, useValue: { paginatedEmployees$: of() } },
+        { provide: ActivatedRoute, useValue: { queryParamMap: of() } }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
