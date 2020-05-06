bf920a055f36950b9a5e7d360ef55e840d8624cb
Johan Alvarez <llstarscreamll@hotmail.com>
2020-05-06 18:49:07 -0500

    feat(time-clock-logs): search sub cost center only by code and sort by code

--- libs/time-clock-logs/feature/src/lib/entry-and-exit-log-page/entry-and-exit-log-page.component.ts
+++ libs/time-clock-logs/feature/src/lib/entry-and-exit-log-page/entry-and-exit-log-page.component.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { TimeClockLogsFacade } from "@kirby/time-clock-logs/data-access";
+import { TimeClockLogsFacade } from '@kirby/time-clock-logs/data-access';
 
 @Component({
   selector: 'kirby-entry-and-exit-log-page',
@@ -9,13 +9,12 @@ import { TimeClockLogsFacade } from "@kirby/time-clock-logs/data-access";
   styleUrls: ['./entry-and-exit-log-page.component.scss']
 })
 export class EntryAndExitLogPageComponent implements OnInit, OnDestroy {
-
   public apiError$: Observable<any>;
   public creatingStatus$: Observable<any>;
   public subCostCenters$: Observable<any>;
   public employeeTimeClockData$: Observable<any>;
 
-  constructor(private timeClockLogsFacade: TimeClockLogsFacade) { }
+  constructor(private timeClockLogsFacade: TimeClockLogsFacade) {}
 
   ngOnInit(): void {
     this.apiError$ = this.timeClockLogsFacade.apiError$;
@@ -37,7 +36,10 @@ export class EntryAndExitLogPageComponent implements OnInit, OnDestroy {
   }
 
   onSearchSubCostCenters(code: any) {
-    this.timeClockLogsFacade.searchSubCostCenters(code);
+    this.timeClockLogsFacade.searchSubCostCenters({
+      ...code,
+      orderBy: 'code',
+      searchFields: 'code:like'
+    });
   }
-
 }
