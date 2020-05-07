e10e42281e3f84285153a4a5d463ae588b61a9aa
Johan Alvarez <llstarscreamll@hotmail.com>
2020-05-06 19:45:04 -0500

    feat(shared): create custom form validator for object type like selections

--- libs/shared/src/lib/validators/index.spec.ts
+++ libs/shared/src/lib/validators/index.spec.ts
@@ -6,4 +6,15 @@ describe('custom validators', () => {
     const control = new FormControl({ id: 1, name: 'John' });
     expect(isObject(control)).toBeNull;
   });
+
+  it('should return object when form field value is NOT an object', () => {
+    const string = new FormControl('john');
+    expect(isObject(string)).toMatchObject({isObject: 'value is not object'});
+
+    const emptyString = new FormControl('');
+    expect(isObject(emptyString)).toMatchObject({isObject: 'value is not object'});
+
+    const nullValue = new FormControl(null);
+    expect(isObject(nullValue)).toMatchObject({isObject: 'value is not object'});
+  });
 });
