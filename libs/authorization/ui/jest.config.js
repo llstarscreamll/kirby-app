900b19a32de2dd2a108345fda7535148233bc026
Johan Alvarez <llstarscreamll@hotmail.com>
2020-05-11 21:01:11 -0500

    test(authorization): fix jest setup

--- libs/authorization/ui/jest.config.js
+++ libs/authorization/ui/jest.config.js
@@ -2,8 +2,4 @@ module.exports = {
   name: 'authorization-ui',
   preset: '../../../jest.config.js',
   coverageDirectory: '../../../coverage/libs/authorization/ui',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
 };
