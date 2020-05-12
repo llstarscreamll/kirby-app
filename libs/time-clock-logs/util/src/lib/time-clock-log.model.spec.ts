a0574d73a76130d14c0045b29c9d8e3115bbc45d
Johan Alvarez <llstarscreamll@hotmail.com>
2020-05-11 20:56:06 -0500

    test: make time-clock-log.model.spec.ts test green

--- libs/time-clock-logs/util/src/lib/time-clock-log.model.spec.ts
+++ libs/time-clock-logs/util/src/lib/time-clock-log.model.spec.ts
@@ -1,1 +1,29 @@
-describe('TimeClockLogModel', () => {});
+import { createUser } from '@kirby/users/testing/src';
+import { createTimeClockLog } from '@kirby/time-clock-logs/testing';
+
+describe('TimeClockLogModel', () => {
+  describe('isApprovedByUserId', () => {
+    it('should return false when approvals is null', () => {
+      const model = createTimeClockLog();
+      const user = createUser();
+
+      expect(model.isApprovedByUserId(user.id)).toBe(false);
+    });
+
+    it('should return false when approvals is empty array', () => {
+      const model = createTimeClockLog();
+      const user = createUser();
+      model.approvals = [];
+
+      expect(model.isApprovedByUserId(user.id)).toBe(false);
+    });
+
+    it('should return true when argument exists as approval', () => {
+      const model = createTimeClockLog();
+      const user = createUser();
+      model.approvals = [user];
+
+      expect(model.isApprovedByUserId(user.id)).toBe(true);
+    });
+  });
+});
