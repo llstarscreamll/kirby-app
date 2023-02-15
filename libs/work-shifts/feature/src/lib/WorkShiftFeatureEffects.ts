b3f4a8f7405bd5f19b51ff55f87a4ac79b7c48b8
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-14 22:38:45 -0500

    fix: migrate to new data persistence operator from NX

--- libs/work-shifts/feature/src/lib/WorkShiftFeatureEffects.ts
+++ libs/work-shifts/feature/src/lib/WorkShiftFeatureEffects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { DataPersistence } from '@nrwl/angular';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { navigation } from '@nrwl/angular';
+import { Actions, createEffect } from '@ngrx/effects';
 
 import { WorkShiftsFacade } from '@kirby/work-shifts/data-access';
 import { EditWorkShiftPage } from './edit-work-shift-page/edit-work-shift.page';
@@ -8,14 +8,12 @@ import { EditWorkShiftPage } from './edit-work-shift-page/edit-work-shift.page';
 @Injectable()
 export class WorkShiftFeatureEffects {
   editWorkShiftPage$ = createEffect(() =>
-    this.dataPersistence.navigation(EditWorkShiftPage, {
-      run: (routeSnapshot) =>
-        this.workShiftsFacade.get(routeSnapshot.params['id']),
-    })
+    this.actions$.pipe(
+      navigation(EditWorkShiftPage, {
+        run: (routeSnapshot) => this.workShiftsFacade.get(routeSnapshot.params['id']),
+      })
+    )
   );
 
-  constructor(
-    private workShiftsFacade: WorkShiftsFacade,
-    private dataPersistence: DataPersistence<any>
-  ) {}
+  constructor(private actions$: Actions, private workShiftsFacade: WorkShiftsFacade) {}
 }
