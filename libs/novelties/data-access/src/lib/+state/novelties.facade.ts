d15f9663610ca9d74b4271b58cfe8898f5c9a51c
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-16 09:54:28 -0500

    feat(novelties): migrate facades to new ngrx creators API

--- libs/novelties/data-access/src/lib/+state/novelties.facade.ts
+++ libs/novelties/data-access/src/lib/+state/novelties.facade.ts
@@ -1,81 +1,52 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
+import { User } from '@kirby/users/util';
+
+import * as a from './novelties.actions';
 import { noveltiesQuery } from './novelties.selectors';
 import { NoveltiesPartialState } from './novelties.reducer';
-import {
-  SearchNovelties,
-  GetNovelty,
-  SearchNoveltyTypes,
-  UpdateNovelty,
-  CleanSelectedNovelty,
-  CreateNoveltiesToEmployees,
-  CleanApiErrors,
-  ResetCreateNoveltiesToEmployees,
-  ApproveNovelty,
-  DeleteNoveltyApproval,
-  TrashNovelty,
-  SetApprovalsByEmployeeAndDateRange,
-  DeleteApprovalsByEmployeeAndDateRange,
-  DownLoadNoveltiesReport,
-  SearchNoveltiesOk,
-  GetResume,
-  CreateBalanceNovelty,
-  ExportResume,
-} from './novelties.actions';
-import { User } from '@kirby/users/util';
 
 @Injectable()
 export class NoveltiesFacade {
-  loaded$ = this.store.pipe(select(noveltiesQuery.getLoaded));
   error$ = this.store.pipe(select(noveltiesQuery.getError));
-  createNoveltiesToEmployeesStatus$ = this.store.pipe(
-    select(noveltiesQuery.getCreateNoveltiesToEmployeesStatus)
-  );
-  paginatedNovelties$ = this.store.pipe(
-    select(noveltiesQuery.getPaginatedList)
-  );
-  paginatedNoveltyTypes$ = this.store.pipe(
-    select(noveltiesQuery.getPaginatedNoveltyTypesList)
-  );
+  loaded$ = this.store.pipe(select(noveltiesQuery.getLoaded));
+  paginatedNovelties$ = this.store.pipe(select(noveltiesQuery.getPaginatedList));
   selectedNovelty$ = this.store.pipe(select(noveltiesQuery.getSelectedNovelty));
-  reportByEmployee$ = this.store.pipe(
-    select(noveltiesQuery.getReportByEmployee)
-  );
-
-  resumeByEmployeesAndNoveltyTypes$ = this.store.pipe(
-    select(noveltiesQuery.getResumeByEmployeesAndNoveltyTypes)
-  );
+  reportByEmployee$ = this.store.pipe(select(noveltiesQuery.getReportByEmployee));
+  paginatedNoveltyTypes$ = this.store.pipe(select(noveltiesQuery.getPaginatedNoveltyTypesList));
+  createNoveltiesToEmployeesStatus$ = this.store.pipe(select(noveltiesQuery.getCreateNoveltiesToEmployeesStatus));
+  resumeByEmployeesAndNoveltyTypes$ = this.store.pipe(select(noveltiesQuery.getResumeByEmployeesAndNoveltyTypes));
 
   constructor(private store: Store<NoveltiesPartialState>) {}
 
   search(query: any = {}) {
-    this.store.dispatch(new SearchNovelties(query));
+    this.store.dispatch(a.Search(query));
   }
 
   get(noveltyId: string) {
-    this.store.dispatch(new GetNovelty(noveltyId));
+    this.store.dispatch(a.Get({ payload: { noveltyId } }));
   }
 
   cleanNoveltiesSearch() {
-    this.store.dispatch(new CleanApiErrors());
-    this.store.dispatch(new SearchNoveltiesOk(null));
+    this.store.dispatch(a.CleanErrors());
+    this.store.dispatch(a.SearchOk({ payload: null }));
   }
 
   update(noveltyId: string, noveltyData) {
-    this.store.dispatch(new UpdateNovelty({ id: noveltyId, noveltyData }));
+    this.store.dispatch(a.Edit({ payload: { id: noveltyId, noveltyData } }));
   }
 
   trash(noveltyId: string) {
-    this.store.dispatch(new TrashNovelty(noveltyId));
+    this.store.dispatch(a.Trash({ payload: noveltyId }));
   }
 
   createNoveltiesToEmployees(data) {
-    this.store.dispatch(new CreateNoveltiesToEmployees(data));
+    this.store.dispatch(a.Create({ payload: data }));
   }
 
   createBalanceNovelty(data) {
-    this.store.dispatch(new CreateBalanceNovelty(data));
+    this.store.dispatch(a.CreateBalance({ payload: data }));
   }
 
   /**
@@ -83,66 +54,54 @@ export class NoveltiesFacade {
    * @param query
    */
   searchNoveltyTypes(query: any = {}) {
-    this.store.dispatch(new SearchNoveltyTypes(query));
+    this.store.dispatch(a.SearchNoveltyTypes({ payload: query }));
   }
 
   cleanSelected() {
-    this.store.dispatch(new CleanSelectedNovelty());
+    this.store.dispatch(a.CleanSelected());
   }
 
   resetCreateNoveltiesToEmployees() {
-    this.store.dispatch(new ResetCreateNoveltiesToEmployees());
+    this.store.dispatch(a.ResetCreate());
   }
 
   cleanApiErrors() {
-    this.store.dispatch(new CleanApiErrors());
+    this.store.dispatch(a.CleanErrors());
   }
 
   approve(noveltyId: string, user: User) {
-    this.store.dispatch(new ApproveNovelty({ noveltyId, user }));
+    this.store.dispatch(a.Approve({ payload: { noveltyId, user } }));
   }
 
-  setApprovalsByEmployeeAndDateRange(
-    employeeId: string,
-    startDate: string,
-    endDate: string
-  ) {
-    this.store.dispatch(
-      new SetApprovalsByEmployeeAndDateRange({ employeeId, startDate, endDate })
-    );
+  setApprovalsByEmployeeAndDateRange(employeeId: string, startDate: string, endDate: string) {
+    this.store.dispatch(a.SetApprovals({ payload: { employeeId, startDate, endDate } }));
   }
 
   deleteNoveltyApproval(noveltyId: string, user: User) {
-    this.store.dispatch(new DeleteNoveltyApproval({ noveltyId, user }));
+    this.store.dispatch(a.DeleteApproval({ payload: { noveltyId, user } }));
   }
 
-  deleteApprovalsByEmployeeAndDateRange(
-    employeeId: string,
-    startDate: string,
-    endDate: string
-  ) {
+  deleteApprovalsByEmployeeAndDateRange(employeeId: string, startDate: string, endDate: string) {
     this.store.dispatch(
-      new DeleteApprovalsByEmployeeAndDateRange({
-        employeeId,
-        startDate,
-        endDate,
+      a.DeleteApprovals({
+        payload: {
+          employeeId,
+          startDate,
+          endDate,
+        },
       })
     );
   }
 
-  downloadReport(query: {
-    employee_id?: string;
-    start_date: string;
-    end_date: string;
-  }) {
-    this.store.dispatch(new DownLoadNoveltiesReport(query));
+  downloadReport(query: { employee_id?: string; start_date: string; end_date: string }) {
+    this.store.dispatch(a.DownLoadReport({ payload: query }));
   }
 
   getResumeByEmployeesAndNoveltyTypes(query) {
-    this.store.dispatch(new GetResume(query));
+    this.store.dispatch(a.GetResume({ payload: query }));
   }
 
   exportNoveltiesResume(query) {
-    this.store.dispatch(new ExportResume(query));
+    this.store.dispatch(a.ExportResume({ payload: query }));
   }
 }
