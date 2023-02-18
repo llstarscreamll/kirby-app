1dcf35f2ec26b8421c24541715a1ee40b686a6f0
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-17 23:57:58 -0500

    feat(employees): migrate to new NGRX effects creators

--- libs/employees/feature/src/lib/+state/employee-feature.effects.ts
+++ libs/employees/feature/src/lib/+state/employee-feature.effects.ts
@@ -1,27 +1,22 @@
-import { Observable } from 'rxjs';
-import { Action } from '@ngrx/store';
-import { Effect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import { DataPersistence } from '@nrwl/angular';
+import { navigation } from '@nrwl/angular';
+import { Actions, createEffect } from '@ngrx/effects';
 import { ActivatedRouteSnapshot } from '@angular/router';
 
 import { EmployeesFacade } from '@kirby/employees/data-access';
+
 import { EditEmployeePageComponent } from '../edit-employee-page/edit-employee-page.component';
 
 @Injectable()
 export class EmployeeFeatureEffects {
-  @Effect()
-  getEmployee$: Observable<Action> = this.dataPersistence.navigation(
-    EditEmployeePageComponent,
-    {
-      run: (routerSnapShot: ActivatedRouteSnapshot) =>
-        this.employeesFacade.get(routerSnapShot.params['id']),
-      onError: () => {}
-    }
+  getEmployee$ = createEffect(() =>
+    this.actions$.pipe(
+      navigation(EditEmployeePageComponent, {
+        run: (routerSnapShot: ActivatedRouteSnapshot) => this.employeesFacade.get(routerSnapShot.params['id']),
+        onError: () => {},
+      })
+    )
   );
 
-  constructor(
-    private employeesFacade: EmployeesFacade,
-    private dataPersistence: DataPersistence<any>
-  ) {}
+  constructor(private actions$: Actions, private employeesFacade: EmployeesFacade) {}
 }
