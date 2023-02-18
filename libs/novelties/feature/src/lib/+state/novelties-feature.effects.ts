0554f37ad61b0d55b469a5f980c5618b054875af
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-18 09:41:25 -0500

    feat(novelties): migrate to new NGRX effects creator

--- libs/novelties/feature/src/lib/+state/novelties-feature.effects.ts
+++ libs/novelties/feature/src/lib/+state/novelties-feature.effects.ts
@@ -1,27 +1,22 @@
-import { Effect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import { DataPersistence } from '@nrwl/angular';
+import { navigation } from '@nrwl/angular';
 import { ActivatedRouteSnapshot } from '@angular/router';
+import { Actions, createEffect } from '@ngrx/effects';
 
-import {
-  NoveltiesFacade,
-  NoveltiesPartialState,
-} from '@kirby/novelties/data-access';
+import { NoveltiesFacade, NoveltiesPartialState } from '@kirby/novelties/data-access';
+
 import { EditNoveltyPageComponent } from '../edit-novelty/edit-novelty-page.component';
 
 @Injectable()
 export class NoveltiesFeatureEffects {
-  @Effect() editNoveltyPage$ = this.dataPersistence.navigation(
-    EditNoveltyPageComponent,
-    {
-      run: (routerSnapShot: ActivatedRouteSnapshot) =>
-        this.noveltiesFacade.get(routerSnapShot.params['id']),
-      onError: () => {},
-    }
+  editNoveltyPage$ = createEffect(() =>
+    this.actions$.pipe(
+      navigation(EditNoveltyPageComponent, {
+        run: (routerSnapShot: ActivatedRouteSnapshot) => this.noveltiesFacade.get(routerSnapShot.params['id']),
+        onError: () => {},
+      })
+    )
   );
 
-  constructor(
-    private noveltiesFacade: NoveltiesFacade,
-    private dataPersistence: DataPersistence<NoveltiesPartialState>
-  ) {}
+  constructor(private actions$: Actions, private noveltiesFacade: NoveltiesFacade) {}
 }
