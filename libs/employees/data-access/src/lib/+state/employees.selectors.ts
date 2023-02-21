5f47c824b3e694ec596e13202eef9c59d015499f
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-20 22:43:05 -0500

    style: code formatting

--- libs/employees/data-access/src/lib/+state/employees.selectors.ts
+++ libs/employees/data-access/src/lib/+state/employees.selectors.ts
@@ -2,14 +2,15 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { EMPLOYEES_FEATURE_KEY, EmployeesState } from './employees.reducer';
 
 const getEmployeesState = createFeatureSelector<EmployeesState>(EMPLOYEES_FEATURE_KEY);
-const getPaginated = createSelector(getEmployeesState, (state: EmployeesState) => state.paginatedList);
-const getPaginatingStatus = createSelector(getEmployeesState, (state: EmployeesState) => state.paginatingStatus);
-const getSelectedEmployee = createSelector(getEmployeesState, (state: EmployeesState) => state.selected);
-const getSelectingStatus = createSelector(getEmployeesState, (state: EmployeesState) => state.selectingStatus);
-const getUpdatingStatus = createSelector(getEmployeesState, (state: EmployeesState) => state.updatingStatus);
-const getCreatingStatus = createSelector(getEmployeesState, (state: EmployeesState) => state.creatingStatus);
-const getError = createSelector(getEmployeesState, (state: EmployeesState) => state.error);
-const getRoles = createSelector(getEmployeesState, (state: EmployeesState) => state.roles);
+const selectFeature = (state: any) => state[EMPLOYEES_FEATURE_KEY];
+const getPaginated = createSelector(selectFeature, (state: EmployeesState) => state.paginatedList);
+const getPaginatingStatus = createSelector(selectFeature, (state: EmployeesState) => state.paginatingStatus);
+const getSelectedEmployee = createSelector(selectFeature, (state: EmployeesState) => state.selected);
+const getSelectingStatus = createSelector(selectFeature, (state: EmployeesState) => state.selectingStatus);
+const getUpdatingStatus = createSelector(selectFeature, (state: EmployeesState) => state.updatingStatus);
+const getCreatingStatus = createSelector(selectFeature, (state: EmployeesState) => state.creatingStatus);
+const getError = createSelector(selectFeature, (state: EmployeesState) => state.error);
+const getRoles = createSelector(selectFeature, (state: EmployeesState) => state.roles);
 
 export const employeesQuery = {
   getPaginated,
--- libs/employees/data-access/src/lib/employees-data-access.module.ts
+++ libs/employees/data-access/src/lib/employees-data-access.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { CommonModule } from '@angular/common';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {
-  EMPLOYEES_FEATURE_KEY,
-  initialState as employeesInitialState,
-  employeesReducer
-} from './+state/employees.reducer';
+
 import { EmployeeService } from './employee.service';
-import { EmployeesEffects } from './+state/employees.effects';
 import { EmployeesFacade } from './+state/employees.facade';
+import { EmployeesEffects } from './+state/employees.effects';
+import { EMPLOYEES_FEATURE_KEY, initialState, employeesReducer } from './+state/employees.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
     MatSnackBarModule,
-    StoreModule.forFeature(EMPLOYEES_FEATURE_KEY, employeesReducer, {
-      initialState: employeesInitialState
-    }),
-    EffectsModule.forFeature([EmployeesEffects])
+    StoreModule.forFeature(EMPLOYEES_FEATURE_KEY, employeesReducer, { initialState }),
+    EffectsModule.forFeature([EmployeesEffects]),
   ],
-  providers: [EmployeesFacade, EmployeeService]
+  providers: [EmployeesFacade, EmployeeService],
 })
 export class EmployeesDataAccessModule {}
