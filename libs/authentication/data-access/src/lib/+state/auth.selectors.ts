32dfd1a893bd2a8b74c7dec6f9bf486f6e144397
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-20 22:33:49 -0500

    refactor: cleanup code

--- libs/authentication/data-access/src/lib/+state/auth.selectors.ts
+++ libs/authentication/data-access/src/lib/+state/auth.selectors.ts
@@ -1,15 +1,15 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-
 import { User } from '@kirby/users/util';
-
+import { createSelector } from '@ngrx/store';
 import { AuthState, AUTH_FEATURE_KEY } from './auth.reducer';
 
-const getAuthState = createFeatureSelector<AuthState>(AUTH_FEATURE_KEY);
-const getStatus = createSelector(getAuthState, (state: AuthState) => state.status);
-const getErrors = createSelector(getAuthState, (state: AuthState) => state.errors);
-const getAuthUser = createSelector(getAuthState, (state: AuthState) => (state.user ? User.fromJson(state.user) : null));
-const getAuthTokens = createSelector(getAuthState, (state: AuthState) => state.tokens);
-const getIsLoggedIn = createSelector(getAuthState, (state: AuthState) => state.status === 'loggedIn');
+const selectFeature = (state: any) => state[AUTH_FEATURE_KEY];
+const getStatus = createSelector(selectFeature, (state: AuthState) => state.status);
+const getErrors = createSelector(selectFeature, (state: AuthState) => state.errors);
+const getAuthUser = createSelector(selectFeature, (state: AuthState) =>
+  state.user ? User.fromJson(state.user) : null
+);
+const getAuthTokens = createSelector(selectFeature, (state: AuthState) => state.tokens);
+const getIsLoggedIn = createSelector(selectFeature, (state: AuthState) => state.status);
 
 export const authQuery = {
   getStatus,
