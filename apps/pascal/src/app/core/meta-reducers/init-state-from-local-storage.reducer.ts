8268cccf27484d153e9740b74893ece98c86d9d3
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-20 22:28:25 -0500

    style: format code

--- apps/pascal/src/app/core/meta-reducers/init-state-from-local-storage.reducer.ts
+++ apps/pascal/src/app/core/meta-reducers/init-state-from-local-storage.reducer.ts
@@ -2,14 +2,14 @@ import { ActionReducer, INIT, UPDATE } from '@ngrx/store';
 
 import { LocalStorageService } from '@kirby/shared';
 
-export function initStateFromLocalStorage(
-  reducer: ActionReducer<any>
-): ActionReducer<any> {
+export function initStateFromLocalStorage(reducer: ActionReducer<any>): ActionReducer<any> {
   return function (state, action) {
     const newState = reducer(state, action);
-    if ([INIT.toString(), UPDATE.toString()].includes(action.type)) {
+
+    if ([INIT.toString()].includes(action.type)) {
       return { ...newState, ...LocalStorageService.loadInitialState() };
     }
+
     return newState;
   };
 }
