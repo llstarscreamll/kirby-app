95e3a88747cb9d1d070f16242d13291db31206be
Johan Alvarez <llstarscreamll@hotmail.com>
2023-02-22 22:18:59 -0500

    feat(employees): remove deprecated Angular Flex Layout directives and make visual layout improvemens on create and edit forms

--- libs/employees/ui/src/lib/employees-ui.module.ts
+++ libs/employees/ui/src/lib/employees-ui.module.ts
@@ -1,29 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatButtonModule,
+    MatIconModule,
     MatInputModule,
     MatSelectModule,
-    MatIconModule,
-
+    MatButtonModule,
+    MatTooltipModule,
     MatFormFieldModule,
-    MatAutocompleteModule
+    ReactiveFormsModule,
+    MatAutocompleteModule,
   ],
   declarations: [EmployeeFormComponent],
-  exports: [EmployeeFormComponent]
+  exports: [EmployeeFormComponent],
 })
 export class EmployeesUiModule {}
