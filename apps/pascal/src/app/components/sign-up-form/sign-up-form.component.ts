793c61a11802ef8d4b74706902ef6ff32b561740
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-01 22:05:20 -0500

    refactor: remove unused style file

--- apps/pascal/src/app/components/sign-up-form/sign-up-form.component.ts
+++ apps/pascal/src/app/components/sign-up-form/sign-up-form.component.ts
@@ -7,7 +7,6 @@ import { flatApiErrors } from '@kirby/shared';
 @Component({
   selector: 'pascal-auth-sign-up-form',
   templateUrl: './sign-up-form.component.html',
-  styleUrls: ['./sign-up-form.component.scss']
 })
 export class SignUpFormComponent implements OnInit {
   @Input()
@@ -25,32 +24,11 @@ export class SignUpFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      first_name: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(150)
-        ]
-      ],
-      last_name: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(150)
-        ]
-      ],
+      first_name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(150)]],
+      last_name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(150)]],
       email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.maxLength(150)
-        ]
-      ],
-      password_confirmation: ['', [Validators.required]]
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(150)]],
+      password_confirmation: ['', [Validators.required]],
     });
   }
 
