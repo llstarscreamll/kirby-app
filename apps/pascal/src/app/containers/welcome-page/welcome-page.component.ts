0ade6a0bc32d0ee0281eec87755e7eee648b6bcc
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-01 22:11:36 -0500

    refactor: remove unused style file

--- apps/pascal/src/app/containers/welcome-page/welcome-page.component.ts
+++ apps/pascal/src/app/containers/welcome-page/welcome-page.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthFacade } from '@kirby/authentication/data-access';
 import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthFacade } from '@kirby/authentication/data-access';
 
 @Component({
   selector: 'pascal-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
-
   user$: Observable<any>;
 
-  constructor(private authFacade: AuthFacade) { }
+  constructor(private authFacade: AuthFacade) {}
 
   ngOnInit() {
     this.user$ = this.authFacade.authUser$;
   }
-
 }
