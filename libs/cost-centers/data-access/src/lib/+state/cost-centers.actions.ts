8b4e71c8dbe745e60485e286cb1ca9de77153fb7
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-15 22:09:24 -0500

    feat(cost-centers): migrate to new NGRX action creators

--- libs/cost-centers/data-access/src/lib/+state/cost-centers.actions.ts
+++ libs/cost-centers/data-access/src/lib/+state/cost-centers.actions.ts
@@ -1,35 +1,12 @@
-import { Action } from '@ngrx/store';
+import { Action, createActionGroup, emptyProps } from '@ngrx/store';
 import { Pagination, ApiError } from '@kirby/shared';
 import { CostCenter } from '@kirby/cost-centers/data';
 
-export enum CostCentersActionTypes {
-  SearchCostCenters = '[CostCenters] search',
-  SearchCostCentersOk = '[CostCenters] search ok',
-  SearchCostCentersError = '[CostCenters] search error'
-}
-
-export class SearchCostCenters implements Action {
-  readonly type = CostCentersActionTypes.SearchCostCenters;
-  constructor(public payload: any) {}
-}
-
-export class SearchCostCentersOk implements Action {
-  readonly type = CostCentersActionTypes.SearchCostCentersOk;
-  constructor(public payload: Pagination<CostCenter>) {}
-}
-
-export class SearchCostCentersError implements Action {
-  readonly type = CostCentersActionTypes.SearchCostCentersError;
-  constructor(public payload: ApiError) {}
-}
-
-export type CostCentersAction =
-  | SearchCostCenters
-  | SearchCostCentersOk
-  | SearchCostCentersError;
-
-export const fromCostCentersActions = {
-  SearchCostCenters,
-  SearchCostCentersOk,
-  SearchCostCentersError
-};
+export const costCentersActions = createActionGroup({
+  source: 'costCenters',
+  events: {
+    'search': (payload: any) => ({ payload }),
+    'search ok': (payload: Pagination<CostCenter>) => ({ payload }),
+    'search error': (payload: ApiError) => ({ payload }),
+  },
+});
