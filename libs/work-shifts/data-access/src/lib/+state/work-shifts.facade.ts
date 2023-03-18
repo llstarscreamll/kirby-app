281728e7d1cd762d0ee79b5bd35641e5dcd42850
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-17 23:29:56 -0500

    refactor(work-shifts): remove boilerplate code simplifying store selectors exports

--- libs/work-shifts/data-access/src/lib/+state/work-shifts.facade.ts
+++ libs/work-shifts/data-access/src/lib/+state/work-shifts.facade.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
-import { workShiftsQuery } from './work-shifts.selectors';
+import * as selectors from './work-shifts.selectors';
 import { WorkShiftsPartialState } from './work-shifts.reducer';
 import { WorkShiftInterface } from '@kirby/work-shifts/util';
 import { workShiftsActionTypes as actions } from './work-shifts.actions';
 
 @Injectable()
 export class WorkShiftsFacade {
-  paginatedWorkShifts$ = this.store.pipe(select(workShiftsQuery.getPaginatedWorkShifts));
-  getWorkShiftsList$ = this.store.pipe(select(workShiftsQuery.getWorkShiftsList));
-  paginatingStatus$ = this.store.pipe(select(workShiftsQuery.paginatingStatus));
-  creatingStatus$ = this.store.pipe(select(workShiftsQuery.creatingStatus));
-  selectedWorkShift$ = this.store.pipe(select(workShiftsQuery.getSelectedWorkShift));
-  selectingStatus$ = this.store.pipe(select(workShiftsQuery.selectingStatus));
-  updatingStatus$ = this.store.pipe(select(workShiftsQuery.updatingStatus));
-  deletingStatus$ = this.store.pipe(select(workShiftsQuery.deletingStatus));
-  error$ = this.store.pipe(select(workShiftsQuery.getError));
+  paginatedWorkShifts$ = this.store.pipe(select(selectors.getPaginatedWorkShifts));
+  getWorkShiftsList$ = this.store.pipe(select(selectors.getWorkShiftsList));
+  paginatingStatus$ = this.store.pipe(select(selectors.paginatingStatus));
+  creatingStatus$ = this.store.pipe(select(selectors.creatingStatus));
+  selectedWorkShift$ = this.store.pipe(select(selectors.getSelectedWorkShift));
+  selectingStatus$ = this.store.pipe(select(selectors.selectingStatus));
+  updatingStatus$ = this.store.pipe(select(selectors.updatingStatus));
+  deletingStatus$ = this.store.pipe(select(selectors.deletingStatus));
+  error$ = this.store.pipe(select(selectors.getError));
 
   constructor(private store: Store<WorkShiftsPartialState>) {}
 
--- libs/work-shifts/data-access/src/lib/+state/work-shifts.selectors.spec.ts
+++ libs/work-shifts/data-access/src/lib/+state/work-shifts.selectors.spec.ts
@@ -1,7 +1,7 @@
 import { createWorkShift } from '@kirby/work-shifts/testing';
 import { ApiError, LoadStatus, emptyPagination } from '@kirby/shared';
 
-import { workShiftsQuery } from './work-shifts.selectors';
+import * as selectors from './work-shifts.selectors';
 import { WorkShiftsPartialState } from './work-shifts.reducer';
 
 describe('WorkShifts Selectors', () => {
@@ -37,49 +37,49 @@ describe('WorkShifts Selectors', () => {
 
   describe('WorkShifts Selectors', () => {
     it('getPaginatedWorkShifts() should return the paginated list of entities', () => {
-      const results = workShiftsQuery.getPaginatedWorkShifts(storeState);
+      const results = selectors.getPaginatedWorkShifts(storeState);
 
       expect(results.data.length).toBe(3);
     });
 
     it('getSelectedWorkShift() should return the selected entity', () => {
-      const result = workShiftsQuery.getSelectedWorkShift(storeState);
+      const result = selectors.getSelectedWorkShift(storeState);
 
       expect(result.id).toBe('DDD');
     });
 
     it("paginatingStatus() should return the current 'paginating' status", () => {
-      const result = workShiftsQuery.paginatingStatus(storeState);
+      const result = selectors.paginatingStatus(storeState);
 
       expect(result).toBe(LoadStatus.Completed);
     });
 
     it("creatingStatus() should return the current 'creating' status", () => {
-      const result = workShiftsQuery.creatingStatus(storeState);
+      const result = selectors.creatingStatus(storeState);
 
       expect(result).toBe(LoadStatus.Loading);
     });
 
     it("selectingStatus() should return the current 'selecting' status", () => {
-      const result = workShiftsQuery.selectingStatus(storeState);
+      const result = selectors.selectingStatus(storeState);
 
       expect(result).toBe(LoadStatus.Empty);
     });
 
     it("updatingStatus() should return the current 'updating' status", () => {
-      const result = workShiftsQuery.updatingStatus(storeState);
+      const result = selectors.updatingStatus(storeState);
 
       expect(result).toBe(LoadStatus.Error);
     });
 
     it("deletingStatus() should return the current 'deleting' status", () => {
-      const result = workShiftsQuery.deletingStatus(storeState);
+      const result = selectors.deletingStatus(storeState);
 
       expect(result).toBe(LoadStatus.Completed);
     });
 
     it("getError() should return the current 'error' storeState", () => {
-      const result = workShiftsQuery.getError(storeState);
+      const result = selectors.getError(storeState);
 
       expect(result).toBe(ERROR_MSG);
     });
