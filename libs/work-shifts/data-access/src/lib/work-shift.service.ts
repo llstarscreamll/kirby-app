a60dcad4b386f6f3bbccf866a86a82880647246c
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-17 23:30:55 -0500

    style: format code

--- libs/work-shifts/data-access/src/lib/work-shift.service.ts
+++ libs/work-shifts/data-access/src/lib/work-shift.service.ts
@@ -9,36 +9,44 @@ import { WorkShiftInterface } from '@kirby/work-shifts/util';
 
 @Injectable()
 export class WorkShiftService extends BaseAuthService {
-
   private endpoint = this.env.api + 'api/v1/work-shifts/';
 
   constructor(
     @Inject('environment')
     private env,
     private http: HttpClient
-  ) { super(); }
+  ) {
+    super();
+  }
 
   search(query: any = {}): Observable<Pagination<WorkShiftInterface>> {
-    return this.http.get<Pagination<WorkShiftInterface>>(this.endpoint, { headers: this.defaultHeaders, params: query });
+    return this.http.get<Pagination<WorkShiftInterface>>(this.endpoint, {
+      headers: this.defaultHeaders,
+      params: query,
+    });
   }
 
   create(workShiftData: any): Observable<WorkShiftInterface> {
-    return this.http.post<ApiResponse<WorkShiftInterface>>(this.endpoint, workShiftData, { headers: this.defaultHeaders })
-      .pipe(map(response => response.data));
+    return this.http
+      .post<ApiResponse<WorkShiftInterface>>(this.endpoint, workShiftData, { headers: this.defaultHeaders })
+      .pipe(map((response) => response.data));
   }
 
   get(workShiftId: string): Observable<WorkShiftInterface> {
-    return this.http.get<ApiResponse<WorkShiftInterface>>(this.endpoint + workShiftId, { headers: this.defaultHeaders })
-      .pipe(map(response => response.data));
+    return this.http
+      .get<ApiResponse<WorkShiftInterface>>(this.endpoint + workShiftId, { headers: this.defaultHeaders })
+      .pipe(map((response) => response.data));
   }
 
   update(workShiftId: string, workShiftData: any): Observable<WorkShiftInterface> {
-    return this.http.put<ApiResponse<WorkShiftInterface>>(this.endpoint + workShiftId, workShiftData, { headers: this.defaultHeaders })
-      .pipe(map(response => response.data));
+    return this.http
+      .put<ApiResponse<WorkShiftInterface>>(this.endpoint + workShiftId, workShiftData, {
+        headers: this.defaultHeaders,
+      })
+      .pipe(map((response) => response.data));
   }
 
   delete(workShiftId: string): Observable<any> {
     return this.http.delete(this.endpoint + workShiftId, { headers: this.defaultHeaders });
   }
-
 }
