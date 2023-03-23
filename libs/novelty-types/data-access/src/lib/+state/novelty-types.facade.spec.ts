91046291760cd0fbc9080d4d2ec634759156c6fd
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-22 22:06:04 -0500

    tests(novelty-types): make facade tests green and remove deprecated testing helpers

--- libs/novelty-types/data-access/src/lib/+state/novelty-types.facade.spec.ts
+++ libs/novelty-types/data-access/src/lib/+state/novelty-types.facade.spec.ts
@@ -1,30 +1,18 @@
 import { NgModule } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { TestBed } from '@angular/core/testing';
 import { StoreModule, Store } from '@ngrx/store';
 import { readFirst } from '@nrwl/angular/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-import {
-  reducer,
-  initialState,
-  NoveltyTypesState,
-} from './novelty-types.reducer';
+import { createNoveltyType } from '@kirby/novelty-types/testing';
+
 import { NoveltyTypesFacade } from './novelty-types.facade';
 import { NoveltyTypeService } from '../novelty-type.service';
-import {
-  SearchNoveltyTypes,
-  CreateNoveltyType,
-  GetNoveltyType,
-  UpdateNoveltyType,
-  GetNoveltyTypeOk,
-  SetError,
-  TrashNoveltyType,
-} from './novelty-types.actions';
+import { noveltyTypesActions as a } from './novelty-types.actions';
 import { NoveltyTypesEffects } from './novelty-types.effects';
-import { createNoveltyType } from '@kirby/novelty-types/testing';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { reducer, NoveltyTypesState } from './novelty-types.reducer';
 
 interface TestSchema {
   noveltyTypes: NoveltyTypesState;
@@ -37,10 +25,7 @@ describe('NoveltyTypesFacade', () => {
   describe('used in NgModule', () => {
     beforeEach(() => {
       @NgModule({
-        imports: [
-          StoreModule.forFeature('noveltyTypes', reducer, { initialState }),
-          EffectsModule.forFeature([NoveltyTypesEffects]),
-        ],
+        imports: [StoreModule.forFeature(reducer), EffectsModule.forFeature([NoveltyTypesEffects])],
         providers: [
           NoveltyTypesFacade,
           { provide: Router, useValue: { navigate: (u) => true } },
@@ -51,12 +36,7 @@ describe('NoveltyTypesFacade', () => {
       class CustomFeatureModule {}
 
       @NgModule({
-        imports: [
-
-          StoreModule.forRoot({}),
-          EffectsModule.forRoot([]),
-          CustomFeatureModule,
-        ],
+        imports: [StoreModule.forRoot({}), EffectsModule.forRoot([]), CustomFeatureModule],
       })
       class RootModule {}
       TestBed.configureTestingModule({ imports: [RootModule] });
@@ -65,112 +45,86 @@ describe('NoveltyTypesFacade', () => {
       facade = TestBed.inject(NoveltyTypesFacade);
     });
 
-    it('paginatedNoveltyTypes$ should return empty paginated list by default', async (done) => {
-      try {
-        let page = await readFirst(facade.paginatedNoveltyTypes$);
+    it('paginatedNoveltyTypes$ should return empty paginated list by default', async () => {
+      let page = await readFirst(facade.paginatedNoveltyTypes$);
 
-        expect(page.data.length).toBe(0);
-
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+      expect(page.data.length).toBe(0);
     });
 
-    it('selectNoveltyType$ should return selected novelty type', async (done) => {
-      try {
-        let selected = await readFirst(facade.selectedNoveltyType$);
+    it('selectNoveltyType$ should return selected novelty type', async () => {
+      let selected = await readFirst(facade.selectedNoveltyType$);
 
-        expect(selected).toBeFalsy();
+      expect(selected).toBeFalsy();
 
-        const noveltyType = createNoveltyType('AAA');
-        store.dispatch(new GetNoveltyTypeOk(noveltyType));
+      const noveltyType = createNoveltyType('AAA');
+      store.dispatch(a.getOk(noveltyType));
 
-        selected = await readFirst(facade.selectedNoveltyType$);
+      selected = await readFirst(facade.selectedNoveltyType$);
 
-        expect(selected).toBeTruthy();
-        expect(selected.id).toBe('AAA');
-
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+      expect(selected).toBeTruthy();
+      expect(selected.id).toBe('AAA');
     });
 
-    it('errors$ should return current errors', async (done) => {
-      try {
-        let error = await readFirst(facade.errors$);
+    it('errors$ should return current errors', async () => {
+      let error = await readFirst(facade.errors$);
 
-        expect(error).toBeFalsy();
+      expect(error).toBeFalsy();
 
-        const expectedError = { message: 'Crap!!', ok: false };
-        store.dispatch(new SetError(expectedError));
+      const expectedError = { message: 'Crap!!', ok: false };
+      store.dispatch(a.setError(expectedError));
 
-        error = await readFirst(facade.errors$);
+      error = await readFirst(facade.errors$);
 
-        expect(error).toBeTruthy();
-        expect(error.message).toBe('Crap!!');
-
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+      expect(error).toBeTruthy();
+      expect(error.message).toBe('Crap!!');
     });
 
     it('search(...) should call SearchNoveltyTypes action', () => {
       const query = { search: 'foo' };
-     jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'dispatch');
 
       facade.search(query);
 
-      expect(store.dispatch).toHaveBeenCalledWith(
-        new SearchNoveltyTypes(query)
-      );
+      expect(store.dispatch).toHaveBeenCalledWith(a.search(query));
     });
 
     it('get(...) should call GetNoveltyType action', () => {
       const noveltyTypeId = 'AAA';
-     jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'dispatch');
 
       facade.get(noveltyTypeId);
 
-      expect(store.dispatch).toHaveBeenCalledWith(
-        new GetNoveltyType(noveltyTypeId)
-      );
+      expect(store.dispatch).toHaveBeenCalledWith(a.get(noveltyTypeId));
     });
 
     it('create(...) should call CreateNoveltyType action', () => {
       const data = createNoveltyType();
       delete data.id;
-     jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'dispatch');
 
       facade.create(data);
 
-      expect(store.dispatch).toHaveBeenCalledWith(new CreateNoveltyType(data));
+      expect(store.dispatch).toHaveBeenCalledWith(a.create(data));
     });
 
     it('update(...) should call UpdateNoveltyType action', () => {
       const data = createNoveltyType();
       const noveltyTypeId = data.id;
       delete data.id;
-     jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'dispatch');
 
       facade.update(noveltyTypeId, data);
 
-      expect(store.dispatch).toHaveBeenCalledWith(
-        new UpdateNoveltyType({ id: noveltyTypeId, data })
-      );
+      expect(store.dispatch).toHaveBeenCalledWith(a.update({ id: noveltyTypeId, data }));
     });
 
     it('trash(...) should call DeleteNoveltyType action', () => {
       const noveltyTypeId = 'AAA';
-     jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'dispatch');
 
       facade.trash(noveltyTypeId);
 
-      expect(store.dispatch).toHaveBeenCalledWith(
-        new TrashNoveltyType(noveltyTypeId)
-      );
+      expect(store.dispatch).toHaveBeenCalledWith(a.trash(noveltyTypeId));
     });
   });
 });
