dec517456f21ad2d505bb8f7c28e32299735e231
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-22 21:28:00 -0500

    feat(novelty-types): migrate to NGRX action group creators on feature actions

--- libs/novelty-types/data-access/src/lib/+state/novelty-types.actions.ts
+++ libs/novelty-types/data-access/src/lib/+state/novelty-types.actions.ts
@@ -1,145 +1,31 @@
-import { Action } from '@ngrx/store';
+import { createActionGroup } from '@ngrx/store';
 
 import { Pagination, ApiError } from '@kirby/shared';
 import { INoveltyType } from '@kirby/novelty-types/data';
 
-export enum NoveltyTypesActionTypes {
-  Search = '[NoveltyTypes] search',
-  SearchOk = '[NoveltyTypes] search ok',
-  SearchError = '[NoveltyTypes] search error',
+export const noveltyTypesActionTypes = createActionGroup({
+  source: 'NoveltyTypes',
+  events: {
+    search: (payload: any) => ({ payload }),
+    'search ok': (payload: Pagination<INoveltyType>) => ({ payload }),
+    'search error': (payload: ApiError) => ({ payload }),
 
-  Get = '[NoveltyTypes] get',
-  GetOk = '[NoveltyTypes] get ok',
-  GetError = '[NoveltyTypes] get error',
+    get: (payload: string) => ({ payload }),
+    'get ok': (payload: INoveltyType) => ({ payload }),
+    'get error': (payload: ApiError) => ({ payload }),
 
-  Create = '[NoveltyTypes] create',
-  CreateOk = '[NoveltyTypes] create ok',
-  CreateError = '[NoveltyTypes] create error',
+    create: (payload: any) => ({ payload }),
+    'create ok': (payload: INoveltyType) => ({ payload }),
+    'create error': (payload: ApiError) => ({ payload }),
 
-  Update = '[NoveltyTypes] update',
-  UpdateOk = '[NoveltyTypes] update ok',
-  UpdateError = '[NoveltyTypes] update error',
+    update: (payload: { id: string; data: any }) => ({ payload }),
+    'update ok': (payload: INoveltyType) => ({ payload }),
+    'update error': (payload: ApiError) => ({ payload }),
 
-  Trash = '[NoveltyTypes] trash',
-  TrashOk = '[NoveltyTypes] trash ok',
-  TrashError = '[NoveltyTypes] trash error',
+    trash: (payload: string) => ({ payload }),
+    'trash ok': (payload: string) => ({ payload }),
+    'trash error': (payload: ApiError) => ({ payload }),
 
-  SetError = '[NoveltyTypes] set error',
-}
-
-export class SearchNoveltyTypes implements Action {
-  readonly type = NoveltyTypesActionTypes.Search;
-  constructor(public payload: any) {}
-}
-
-export class SearchNoveltyTypesOk implements Action {
-  readonly type = NoveltyTypesActionTypes.SearchOk;
-  constructor(public payload: Pagination<INoveltyType>) {}
-}
-
-export class SearchNoveltyTypesError implements Action {
-  readonly type = NoveltyTypesActionTypes.SearchError;
-  constructor(public payload: ApiError) {}
-}
-
-export class GetNoveltyType implements Action {
-  readonly type = NoveltyTypesActionTypes.Get;
-  constructor(public payload: string) {}
-}
-
-export class GetNoveltyTypeOk implements Action {
-  readonly type = NoveltyTypesActionTypes.GetOk;
-  constructor(public payload: INoveltyType) {}
-}
-
-export class GetNoveltyTypeError implements Action {
-  readonly type = NoveltyTypesActionTypes.GetError;
-  constructor(public payload: ApiError) {}
-}
-
-export class CreateNoveltyType implements Action {
-  readonly type = NoveltyTypesActionTypes.Create;
-  constructor(public payload: any) {}
-}
-
-export class CreateNoveltyTypeOk implements Action {
-  readonly type = NoveltyTypesActionTypes.CreateOk;
-  constructor(public payload: INoveltyType) {}
-}
-
-export class CreateNoveltyTypeError implements Action {
-  readonly type = NoveltyTypesActionTypes.CreateError;
-  constructor(public payload: ApiError) {}
-}
-
-export class UpdateNoveltyType implements Action {
-  readonly type = NoveltyTypesActionTypes.Update;
-  constructor(public payload: { id: string; data: any }) {}
-}
-
-export class UpdateNoveltyTypeOk implements Action {
-  readonly type = NoveltyTypesActionTypes.UpdateOk;
-  constructor(public payload: INoveltyType) {}
-}
-
-export class UpdateNoveltyTypeError implements Action {
-  readonly type = NoveltyTypesActionTypes.UpdateError;
-  constructor(public payload: ApiError) {}
-}
-
-export class TrashNoveltyType implements Action {
-  readonly type = NoveltyTypesActionTypes.Trash;
-  constructor(public payload: string) {}
-}
-
-export class TrashNoveltyTypeOk implements Action {
-  readonly type = NoveltyTypesActionTypes.TrashOk;
-  constructor(public payload: string) {}
-}
-
-export class TrashNoveltyTypeError implements Action {
-  readonly type = NoveltyTypesActionTypes.TrashError;
-  constructor(public payload: ApiError) {}
-}
-
-export class SetError implements Action {
-  readonly type = NoveltyTypesActionTypes.SetError;
-  constructor(public payload: ApiError) {}
-}
-
-export type NoveltyTypesAction =
-  | SearchNoveltyTypes
-  | SearchNoveltyTypesOk
-  | SearchNoveltyTypesError
-  | GetNoveltyType
-  | GetNoveltyTypeOk
-  | GetNoveltyTypeError
-  | CreateNoveltyType
-  | CreateNoveltyTypeOk
-  | CreateNoveltyTypeError
-  | UpdateNoveltyType
-  | UpdateNoveltyTypeOk
-  | UpdateNoveltyTypeError
-  | TrashNoveltyType
-  | TrashNoveltyTypeOk
-  | TrashNoveltyTypeError
-  | SetError;
-
-export const fromNoveltyTypesActions = {
-  SearchNoveltyTypes,
-  SearchNoveltyTypesOk,
-  SearchNoveltyTypesError,
-  GetNoveltyType,
-  GetNoveltyTypeOk,
-  GetNoveltyTypeError,
-  CreateNoveltyType,
-  CreateNoveltyTypeOk,
-  CreateNoveltyTypeError,
-  UpdateNoveltyType,
-  UpdateNoveltyTypeOk,
-  UpdateNoveltyTypeError,
-  TrashNoveltyType,
-  TrashNoveltyTypeOk,
-  TrashNoveltyTypeError,
-  SetError,
-};
+    'set error': (payload: ApiError) => ({ payload }),
+  },
+});
