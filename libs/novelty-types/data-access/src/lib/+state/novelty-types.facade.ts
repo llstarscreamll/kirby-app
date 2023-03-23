fbce6bc85cce091a4b5e0be5be841ab3aa010190
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-22 21:49:05 -0500

    refactor(novelty-types): migrate to NGRX action group creator on Facade class

--- libs/novelty-types/data-access/src/lib/+state/novelty-types.facade.ts
+++ libs/novelty-types/data-access/src/lib/+state/novelty-types.facade.ts
@@ -1,48 +1,38 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
-import {
-  GetNoveltyType,
-  CreateNoveltyType,
-  SearchNoveltyTypes,
-  UpdateNoveltyType,
-  TrashNoveltyType,
-  GetNoveltyTypeOk,
-} from './novelty-types.actions';
 import { noveltyTypesQuery as query } from './novelty-types.selectors';
-import { NoveltyTypesPartialState } from './novelty-types.reducer';
+import { noveltyTypesActions as actions } from './novelty-types.actions';
 
 @Injectable()
 export class NoveltyTypesFacade {
   paginatedNoveltyTypes$ = this.store.pipe(select(query.getPaginated));
-
   selectedNoveltyType$ = this.store.pipe(select(query.getSelected));
-
   errors$ = this.store.pipe(select(query.getError));
 
-  constructor(private store: Store<NoveltyTypesPartialState>) {}
+  constructor(private store: Store) {}
 
   search(query: any = {}) {
-    this.store.dispatch(new SearchNoveltyTypes(query));
+    this.store.dispatch(actions.search(query));
   }
 
   get(noveltyTypeId: string) {
-    this.store.dispatch(new GetNoveltyType(noveltyTypeId));
+    this.store.dispatch(actions.get(noveltyTypeId));
   }
 
   cleanSelected() {
-    this.store.dispatch(new GetNoveltyTypeOk(null));
+    this.store.dispatch(actions.getOk(null));
   }
 
   create(data: any = {}) {
-    this.store.dispatch(new CreateNoveltyType(data));
+    this.store.dispatch(actions.create(data));
   }
 
   update(noveltyTypeId: string, data: any = {}) {
-    this.store.dispatch(new UpdateNoveltyType({ id: noveltyTypeId, data }));
+    this.store.dispatch(actions.update({ id: noveltyTypeId, data }));
   }
 
   trash(noveltyTypeId: string) {
-    this.store.dispatch(new TrashNoveltyType(noveltyTypeId));
+    this.store.dispatch(actions.trash(noveltyTypeId));
   }
 }
