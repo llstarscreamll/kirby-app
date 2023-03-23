ed0a5e39c0fcfee3d9a85fbf10c9b20dcd113d40
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-22 22:08:28 -0500

    tests(novelty-types): make reducer tests green

--- libs/novelty-types/data-access/src/lib/+state/novelty-types.reducer.spec.ts
+++ libs/novelty-types/data-access/src/lib/+state/novelty-types.reducer.spec.ts
@@ -1,26 +1,14 @@
-import {
-  SetError,
-  GetNoveltyTypeOk,
-  TrashNoveltyTypeOk,
-  GetNoveltyTypeError,
-  SearchNoveltyTypesOk,
-  UpdateNoveltyTypeError,
-  CreateNoveltyTypeError,
-  SearchNoveltyTypesError,
-} from './novelty-types.actions';
-import {
-  reducer,
-  initialState,
-  NoveltyTypesState,
-} from './novelty-types.reducer';
 import { emptyPagination } from '@kirby/shared';
 import { createNoveltyType } from '@kirby/novelty-types/testing';
+
+import { noveltyTypesActions as a } from './novelty-types.actions';
+import { reducer, initialState, NoveltyTypesState } from './novelty-types.reducer';
 
 describe('NoveltyTypes Reducer', () => {
   describe('unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
-      const result = reducer(initialState, action);
+      const result = reducer.reducer(initialState, action);
 
       expect(result).toBe(initialState);
     });
@@ -29,32 +17,32 @@ describe('NoveltyTypes Reducer', () => {
   describe('valid NoveltyTypes actions ', () => {
     it('should set paginated list on SearchNoveltyTypesOk action', () => {
       const noveltyTypes = [createNoveltyType('AAA'), createNoveltyType('zzz')];
-      const action = new SearchNoveltyTypesOk({
+      const action = a.searchOk({
         ...emptyPagination(),
         data: noveltyTypes,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.paginatedList.data.length).toBe(2);
     });
 
     it('should set errors on SearchNoveltyTypesError action', () => {
-      const action = new SearchNoveltyTypesError({
+      const action = a.searchError({
         message: 'Crap!!',
         ok: false,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe('Crap!!');
     });
 
     it('should set selected on GetNoveltyTypeOk action', () => {
-      const action = new GetNoveltyTypeOk(createNoveltyType('AAA'));
+      const action = a.getOk(createNoveltyType('AAA'));
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.selected).toBeTruthy();
       expect(result.selected.id).toBe('AAA');
@@ -62,64 +50,64 @@ describe('NoveltyTypes Reducer', () => {
 
     it('should remove novelty type from paginated list on TrashNoveltyTypeOk action', () => {
       // put the novelty type to delete on the paginated list store attribute
-      let result: NoveltyTypesState = reducer(
+      let result: NoveltyTypesState = reducer.reducer(
         initialState,
-        new SearchNoveltyTypesOk({
+        a.searchOk({
           ...emptyPagination(),
           data: [createNoveltyType('AAA')],
         })
       );
       expect(result.paginatedList.data.length).toBe(1);
 
-      const action = new TrashNoveltyTypeOk('AAA');
-      result = reducer(result, action);
+      const action = a.trashOk('AAA');
+      result = reducer.reducer(result, action);
 
       expect(result.paginatedList.data.length).toBe(0);
     });
 
     it('should set errors on GetNoveltyTypeError action', () => {
-      const action = new GetNoveltyTypeError({
+      const action = a.getError({
         message: 'Crap!!',
         ok: false,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe('Crap!!');
     });
 
     it('should set errors on CreateNoveltyTypeError action', () => {
-      const action = new CreateNoveltyTypeError({
+      const action = a.createError({
         message: 'Crap!!',
         ok: false,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe('Crap!!');
     });
 
     it('should set errors on UpdateNoveltyTypeError action', () => {
-      const action = new UpdateNoveltyTypeError({
+      const action = a.updateError({
         message: 'Crap!!',
         ok: false,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe('Crap!!');
     });
 
     it('should set errors on SetError action', () => {
-      const action = new SetError({
+      const action = a.setError({
         message: 'Crap!!',
         ok: false,
       });
 
-      const result: NoveltyTypesState = reducer(initialState, action);
+      const result: NoveltyTypesState = reducer.reducer(initialState, action);
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe('Crap!!');
