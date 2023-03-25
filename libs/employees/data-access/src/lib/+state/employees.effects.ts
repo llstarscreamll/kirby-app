0b96714a7f7067a88c56f8ab978039a8cfde08da
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-24 22:41:57 -0500

    feat(employees): migrate effects to NGRX action group creator

--- libs/employees/data-access/src/lib/+state/employees.effects.ts
+++ libs/employees/data-access/src/lib/+state/employees.effects.ts
@@ -6,67 +6,40 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 
-import {
-  CreateEmployee,
-  CreateEmployeeError,
-  CreateEmployeeOk,
-  EmployeesActionTypes,
-  GetEmployee,
-  GetEmployeeError,
-  GetEmployeeOk,
-  SearchEmployees,
-  SearchEmployeesError,
-  SearchEmployeesOk,
-  SearchRoles,
-  SearchRolesError,
-  SearchRolesOk,
-  UpdateEmployee,
-  UpdateEmployeeError,
-  UpdateEmployeeOk,
-} from './employees.actions';
 import { EmployeeService } from '../employee.service';
-import { EmployeesPartialState } from './employees.reducer';
+import { employeesActions as actions } from './employees.actions';
 
 @Injectable()
 export class EmployeesEffects {
   searchEmployees$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(EmployeesActionTypes.SearchEmployees),
+      ofType(actions.search),
       fetch({
-        run: (action: SearchEmployees, state: EmployeesPartialState) =>
-          this.employeeService.search(action.payload).pipe(map((apiResponse) => new SearchEmployeesOk(apiResponse))),
-
-        onError: (action: SearchEmployees, error) => {
-          return new SearchEmployeesError(error);
-        },
+        run: (action) =>
+          this.employeeService.search(action.payload).pipe(map((apiResponse) => actions.searchOk(apiResponse))),
+        onError: (_, error) => actions.searchError(error),
       })
     )
   );
 
   getEmployee$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(EmployeesActionTypes.GetEmployee),
+      ofType(actions.get),
       fetch({
-        run: (action: GetEmployee, state: EmployeesPartialState) =>
-          this.employeeService.get(action.payload).pipe(map((apiResponse) => new GetEmployeeOk(apiResponse))),
-
-        onError: (action: GetEmployee, error) => {
-          return new GetEmployeeError(error);
-        },
+        run: (action) =>
+          this.employeeService.get(action.payload).pipe(map((apiResponse) => actions.getOk(apiResponse))),
+        onError: (_, error) => actions.getError(error),
       })
     )
   );
 
   createEmployee$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(EmployeesActionTypes.CreateEmployee),
+      ofType(actions.create),
       fetch({
-        run: (action: CreateEmployee, state: EmployeesPartialState) =>
-          this.employeeService.create(action.payload).pipe(map((apiResponse) => new CreateEmployeeOk(apiResponse))),
-
-        onError: (action: CreateEmployee, error) => {
-          return new CreateEmployeeError(error);
-        },
+        run: (action) =>
+          this.employeeService.create(action.payload).pipe(map((apiResponse) => actions.createOk(apiResponse))),
+        onError: (_, error) => actions.createError(error),
       })
     )
   );
@@ -74,7 +47,7 @@ export class EmployeesEffects {
   createEmployeeOk$ = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(EmployeesActionTypes.CreateEmployeeOk),
+        ofType(actions.createOk),
         tap((_) =>
           this.snackBar.open('Empleado creado correctamente', 'Ok', {
             duration: 2000,
@@ -87,16 +60,13 @@ export class EmployeesEffects {
 
   updateEmployee$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(EmployeesActionTypes.UpdateEmployee),
+      ofType(actions.update),
       fetch({
-        run: (action: UpdateEmployee, state: EmployeesPartialState) =>
+        run: (action) =>
           this.employeeService
             .update(action.payload.employeeId, action.payload.data)
-            .pipe(map((apiResponse) => new UpdateEmployeeOk(apiResponse))),
-
-        onError: (action: UpdateEmployee, error) => {
-          return new UpdateEmployeeError(error);
-        },
+            .pipe(map((apiResponse) => actions.updateOk(apiResponse))),
+        onError: (_, error) => actions.updateError(error),
       })
     )
   );
@@ -104,7 +74,7 @@ export class EmployeesEffects {
   updateEmployeeOk$ = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(EmployeesActionTypes.UpdateEmployeeOk),
+        ofType(actions.updateOk),
         tap((_) =>
           this.snackBar.open('Datos actualizados correctamente', 'Ok', {
             duration: 2000,
@@ -117,14 +87,11 @@ export class EmployeesEffects {
 
   searchRoles$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(EmployeesActionTypes.SearchRoles),
+      ofType(actions.searchRoles),
       fetch({
-        run: (action: SearchRoles, state: EmployeesPartialState) =>
-          this.employeeService.searchRoles(action.payload).pipe(map((response) => new SearchRolesOk(response))),
-
-        onError: (action: SearchRoles, error) => {
-          return new SearchRolesError(error);
-        },
+        run: (action) =>
+          this.employeeService.searchRoles(action.payload).pipe(map((response) => actions.searchRolesOk(response))),
+        onError: (_, error) => actions.searchRolesError(error),
       })
     )
   );
