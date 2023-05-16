7bb28eb2688c0a2255421b35a6cb4de3c2cee4b5
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 21:16:53 -0500

    feat(truck-scale): init weighing form component

---
+++ libs/truck-scale/src/lib/components/weighing-form/weighing-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-weighing-form',
+  templateUrl: './weighing-form.component.html',
+  styleUrls: ['./weighing-form.component.scss'],
+})
+export class WeighingFormComponent {}
