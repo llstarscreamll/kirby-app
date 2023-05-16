23e90258e0db0c0fb9417f1e4a6fd810471a63ee
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 21:17:22 -0500

    feat(truck-scale): init create weighing page

---
+++ libs/truck-scale/src/lib/pages/create-weighing/create-weighing.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-create-weighing',
+  templateUrl: './create-weighing.page.html',
+  styleUrls: ['./create-weighing.page.scss'],
+})
+export class CreateWeighingPage {}
