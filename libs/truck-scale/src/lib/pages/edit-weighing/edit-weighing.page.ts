eb69ab04632c382e2e1936eab753975f07b1cdc5
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 21:17:32 -0500

    feat(truck-scale): init edit weighing page

---
+++ libs/truck-scale/src/lib/pages/edit-weighing/edit-weighing.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-edit-weighing',
+  templateUrl: './edit-weighing.page.html',
+  styleUrls: ['./edit-weighing.page.scss'],
+})
+export class EditWeighingPage {}
