2bdf182b7a005a2266252b67eccbced83e239793
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 21:17:57 -0500

    feat(truck-scale): init list weighings page

---
+++ libs/truck-scale/src/lib/pages/weighings-list/weighings-list.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kirby-weighings-list',
+  templateUrl: './weighings-list.page.html',
+  styleUrls: ['./weighings-list.page.scss'],
+})
+export class WeighingsListPage {}
