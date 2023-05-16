641bd1fac4c74feec4651a1cd37edfbadcb0d7d3
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-15 23:02:52 -0500

    feat(truck-scale): declare routes for master, create and edit pages

--- libs/truck-scale/src/lib/lib.routes.ts
+++ libs/truck-scale/src/lib/lib.routes.ts
@@ -1,5 +1,10 @@
 import { Route } from '@angular/router';
+import { WeighingsListPage } from './pages/weighings-list/weighings-list.page';
+import { CreateWeighingPage } from './pages/create-weighing/create-weighing.page';
+import { EditWeighingPage } from './pages/edit-weighing/edit-weighing.page';
 
 export const truckScaleRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+  { path: '', pathMatch: 'full', component: WeighingsListPage },
+  { path: 'create', component: CreateWeighingPage },
+  { path: ':id/edit', component: EditWeighingPage },
 ];
