c023c5ea98cd58869c710dfe38c953bec7bd4d28
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-23 22:45:45 -0500

    feat(truckScale): import and declare modules, services and components

--- libs/truck-scale/src/lib/truck-scale.module.ts
+++ libs/truck-scale/src/lib/truck-scale.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { truckScaleRoutes } from './lib.routes';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+import { SharedModule } from '@kirby/shared';
+import { AuthorizationUiModule } from '@kirby/authorization/ui';
+
+import { truckScaleRoutes } from './lib.routes';
+import { WeighingsService } from './weighings.service';
 import * as fromWeighings from './+state/weighings.reducer';
+import { WeighingsFacade } from './+state/weighings.facade';
 import { WeighingsEffects } from './+state/weighings.effects';
 import { EditWeighingPage } from './pages/edit-weighing/edit-weighing.page';
 import { WeighingsListPage } from './pages/weighings-list/weighings-list.page';
 import { CreateWeighingPage } from './pages/create-weighing/create-weighing.page';
 import { WeighingFormComponent } from './components/weighing-form/weighing-form.component';
-import { SharedModule } from '@kirby/shared';
-import { MatIconModule } from '@angular/material/icon';
-import { AuthorizationUiModule } from '@kirby/authorization/ui';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
@@ -21,11 +29,16 @@ import { MatButtonModule } from '@angular/material/button';
     SharedModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
     AuthorizationUiModule,
+    MatAutocompleteModule,
     RouterModule.forChild(truckScaleRoutes),
     StoreModule.forFeature(fromWeighings.WEIGHINGS_FEATURE_KEY, fromWeighings.weighingsReducer),
     EffectsModule.forFeature([WeighingsEffects]),
   ],
   declarations: [WeighingsListPage, CreateWeighingPage, EditWeighingPage, WeighingFormComponent],
+  providers: [WeighingsService, WeighingsFacade],
 })
 export class TruckScaleModule {}
