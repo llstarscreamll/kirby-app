b818aea900a9494bc261192623de58a74787782b
Johan Alvarez <llstarscreamll@hotmail.com>
2023-05-29 07:17:33 -0500

    feat(truck-scale): set readable names for weighing type, vehicle type and weighing status on list page

--- libs/truck-scale/src/lib/+state/models.ts
+++ libs/truck-scale/src/lib/+state/models.ts
@@ -1,17 +1,42 @@
-export interface Weighing {
-  id: string;
-  weighing_type: string;
-  vehicle_plate: string;
-  vehicle_type: string;
-  driver_dni_number: number;
-  driver_name: string;
-  tare_weight: number;
-  gross_weight: number;
-  weighing_description: string;
-  status: string;
-  created_by_id: string;
-  created_at: string;
-  updated_at: string;
+export class Weighing {
+  readableStatus: string;
+  readableVehicleType: string;
+  readableWeighingType: string;
+
+  constructor(
+    public id: string,
+    public weighing_type: 'load' | 'unload' | 'weighing',
+    public vehicle_plate: string,
+    public vehicle_type: 'one' | 'two',
+    public driver_dni_number: number,
+    public driver_name: string,
+    public tare_weight: number,
+    public gross_weight: number,
+    public weighing_description: string,
+    public status: 'inProgress' | 'finished',
+    public created_by_id: string,
+    public created_at: string,
+    public updated_at: string
+  ) {
+    const readableWeighingTypesLookup = {
+      load: 'Carga',
+      unload: 'Descarga',
+      weighing: 'Pesaje',
+    };
+    this.readableWeighingType = readableWeighingTypesLookup[this.weighing_type] || 'Pesaje';
+
+    const readableVehicleTypeLookup = {
+      one: 'Uno',
+      two: 'Dos',
+    };
+    this.readableVehicleType = readableVehicleTypeLookup[this.vehicle_type] || 'Dos';
+
+    const readableStatusLookup = {
+      inProgress: 'En Progreso',
+      finished: 'Finalizado',
+    };
+    this.readableStatus = readableStatusLookup[this.status] || 'Finished';
+  }
 }
 
 export interface Driver {
--- libs/truck-scale/src/lib/+state/weighings.selectors.ts
+++ libs/truck-scale/src/lib/+state/weighings.selectors.ts
@@ -1,9 +1,29 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { WEIGHINGS_FEATURE_KEY, WeighingsState, adapter } from './weighings.reducer';
+import { Weighing } from './models';
 
 const { selectAll } = adapter.getSelectors();
 const selectWeighingsState = createFeatureSelector<WeighingsState>(WEIGHINGS_FEATURE_KEY);
 export const selectVehicles = createSelector(selectWeighingsState, (state: WeighingsState) => state.vehicles || []);
 export const selectDrivers = createSelector(selectWeighingsState, (state: WeighingsState) => state.drivers || []);
-export const selectWeighings = createSelector(selectWeighingsState, (state: WeighingsState) => selectAll(state));
+export const selectWeighings = createSelector(selectWeighingsState, (state: WeighingsState) =>
+  selectAll(state).map(
+    (w) =>
+      new Weighing(
+        w.id,
+        w.weighing_type,
+        w.vehicle_plate,
+        w.vehicle_type,
+        w.driver_dni_number,
+        w.driver_name,
+        w.tare_weight,
+        w.gross_weight,
+        w.weighing_description,
+        w.status,
+        w.created_by_id,
+        w.created_at,
+        w.updated_at
+      )
+  )
+);
 export const selectError = createSelector(selectWeighingsState, (state: WeighingsState) => state.error);
