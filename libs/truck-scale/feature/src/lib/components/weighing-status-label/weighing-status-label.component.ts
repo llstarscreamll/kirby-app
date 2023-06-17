2b21e62e20660eed46ac7415e12f8adb5a639454
Johan Alvarez <llstarscreamll@hotmail.com>
2023-06-17 10:27:55 -0500

    feat(truck-scale):visual improvements in status label component

--- libs/truck-scale/feature/src/lib/components/weighing-status-label/weighing-status-label.component.ts
+++ libs/truck-scale/feature/src/lib/components/weighing-status-label/weighing-status-label.component.ts
@@ -5,7 +5,7 @@ import { Weighing } from '../../+state/models';
   selector: 'kirby-weighing-status',
   template: `
     <span
-      class="rounded border p-2 mt-3"
+      class="rounded border py-2 px-3 mt-3"
       [ngClass]="{
         'bg-green-300 text-green-700 border-green-400': !weighing?.isFinished(),
         'bg-yellow-300 text-yellow-700 border-yellow-400': weighing?.isFinished()
